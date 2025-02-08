df4ff2346a261aee208ec060daafcb600adf2288
lee-zion <leezion.git@gmail.com>
2025-02-08 18:05:06 +0900

    feat: add drop before create

--- scripts/run_table.py
+++ scripts/run_table.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
 
-from src.utils.create_table import (check_table_exists, connect_with_connector,
-                                    create_table)
+from src.utils.create_table import (
+    check_table_exists,
+    connect_with_connector,
+    create_table,
+    drop_table,
+)
 from src.utils.data_insert import process_excel_and_insert_data
 
 load_dotenv()
@@ -9,6 +13,16 @@ load_dotenv()
 if __name__ == "__main__":
     # Connect to the database
     engine = connect_with_connector()
+    is_drop = input("Drop을 실행할까요? (y/n)")
+    if is_drop.lower() in ["y", "yes"]:
+        is_drop = True
+    else:
+        is_drop = False
+
+    if is_drop and check_table_exists(engine, "report"):
+        drop_table(engine, "embedding")
+        drop_table(engine, "paragraph")
+        drop_table(engine, "report")
 
     # Create tables
     create_table(engine)
--- src/utils/create_table.py
+++ src/utils/create_table.py
@@ -1,11 +1,23 @@
 import os
 
-from google.cloud.sql.connector import Connector, IPTypes
 import pg8000
-
 import sqlalchemy
-from sqlalchemy import Table, Column, MetaData, ForeignKey, VARCHAR, DATE, TEXT, ARRAY, BOOLEAN, UUID, inspect
+from google.cloud.sql.connector import Connector, IPTypes
 from pgvector.sqlalchemy import Vector
+from sqlalchemy import (
+    ARRAY,
+    BOOLEAN,
+    DATE,
+    TEXT,
+    UUID,
+    VARCHAR,
+    Column,
+    ForeignKey,
+    MetaData,
+    Table,
+    inspect,
+)
+
 
 def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     """
@@ -30,7 +42,7 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     # initialize Cloud SQL Python Connector object
     connector = Connector()
 
-    #해당 줄에서 에러가 발생된다면, GCP에서 사용자 추가할 것
+    # 해당 줄에서 에러가 발생된다면, GCP에서 사용자 추가할 것
     def getconn() -> pg8000.dbapi.Connection:
         conn: pg8000.dbapi.Connection = connector.connect(
             instance_connection_name,
@@ -48,7 +60,7 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
         "postgresql+pg8000://",
         creator=getconn,
     )
-    
+
     print("Success Connect!")
     return pool
 
@@ -62,28 +74,34 @@ def create_table(engine):
     report = Table(
         "report",
         metadata,
-        Column("report_id", UUID, primary_key=True),  
-        Column("company_name", VARCHAR(30), nullable=False),  
-        Column("stockfirm_name", VARCHAR(30), nullable=False),  
-        Column("report_date", DATE, nullable=False),  
+        Column("report_id", UUID, primary_key=True),
+        Column("company_name", VARCHAR(30), nullable=False),
+        Column("stockfirm_name", VARCHAR(30), nullable=False),
+        Column("report_date", DATE, nullable=False),
     )
 
     # Paragraph table
     paragraph = Table(
         "paragraph",
         metadata,
-        Column("paragraph_id", UUID, primary_key=True),  
-        Column("report_id", UUID, ForeignKey("report.report_id"), nullable=False),  # Foreign key to report
-        Column("paragraph_text", TEXT, nullable=True),  
+        Column("paragraph_id", UUID, primary_key=True),
+        Column(
+            "report_id", UUID, ForeignKey("report.report_id"), nullable=False
+        ),  # Foreign key to report
+        Column("paragraph_text", TEXT, nullable=True),
     )
 
     # Embedding table
     embedding = Table(
         "embedding",
         metadata,
-        Column("embedding_id", UUID, primary_key=True),  
-        Column("paragraph_id", UUID, ForeignKey("paragraph.paragraph_id"), nullable=False),  # Foreign key to paragraph
-        Column("text_embedding_vector", Vector(1024), nullable=False),  # Embedding vector
+        Column("embedding_id", UUID, primary_key=True),
+        Column(
+            "paragraph_id", UUID, ForeignKey("paragraph.paragraph_id"), nullable=False
+        ),  # Foreign key to paragraph
+        Column(
+            "text_embedding_vector", Vector(1024), nullable=False
+        ),  # Embedding vector
         Column("clean_text_embedding_vector", Vector(1024), nullable=False),
     )
 
@@ -98,5 +116,28 @@ def check_table_exists(engine, table_name):
     tables = inspector.get_table_names()
     if table_name in tables:
         print(f"Table '{table_name}' exists in the database.")
+        return True
+    else:
+        print(f"Table '{table_name}' does not exist in the database.")
+        return False
+
+
+def drop_table(engine, table_name):
+    """Drop a table from the database if it exists."""
+    # Create an inspector to check for the table's existence
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+
+    if table_name in tables:
+        # Create a MetaData object and reflect the table from the engine
+        metadata = MetaData()
+        try:
+            # Reflect the table from the database
+            table = Table(table_name, metadata, autoload_with=engine)
+            # Drop the table
+            table.drop(engine)
+            print(f"Table '{table_name}' dropped successfully!")
+        except sqlalchemy.exc.SQLAlchemyError as e:
+            print(f"Error dropping table '{table_name}': {e}")
     else:
         print(f"Table '{table_name}' does not exist in the database.")
